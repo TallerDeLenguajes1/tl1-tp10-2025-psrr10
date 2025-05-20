0ac3a785d8e00b3cbbb1755ef0941f0557900c07
NievaS24 <santinieva24@gmail.com>
2025-05-19 21:46:45 -0300

    mostrar por pantalla informacion de las tareas

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net.Http;
 using System.Text.Json;
 using Tarea;
@@ -16,7 +17,11 @@ respuesta.EnsureSuccessStatusCode();
 string contenidoJson = await respuesta.Content.ReadAsStringAsync();
 string Json = contenidoJson;
 var tareasDeserializadas = JsonSerializer.Deserialize<List<Tareas>>(Json);
-List<Tareas> tareas = tareasDeserializadas!;
+List<Tareas> listTareas = tareasDeserializadas!;
 
-
+//Mostrar informacion
+foreach(var T in listTareas)
+{
+    Console.WriteLine("Usuario ID:" + T.userId + " " + "ID:" + T.id + " " +"Titulo:" + T.title +" " + "Completado:" + T.completed +"\n");
+}
 
