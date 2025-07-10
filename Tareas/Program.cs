6afea2631e64f018b780460a4f53ab18e3a02f71
Paulo Srur <paulosrur777@gmail.com>
2025-07-09 21:36:49 -0300

    muestro todas las tareas si la peticion fue exitosa

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,2 +1,26 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.Json;
 using Tarea;
+
+
+HttpClient client = new HttpClient();
+
+string url = "https://jsonplaceholder.typicode.com/todos/";
+
+//Envui solicitud GET y verifico que sea exitosa
+HttpResponseMessage respuesta = await client.GetAsync(url);
+respuesta.EnsureSuccessStatusCode();
+
+//Leo y deserializo la respuesta
+string contenidoJson = await respuesta.Content.ReadAsStringAsync();
+var tareasDeserializadas = JsonSerializer.Deserialize<List<Tareas>>(contenidoJson);
+List<Tareas> listTareas = tareasDeserializadas!;
+
+//muestro todas las tareas
+Console.WriteLine("------Todas las tareas------");
+foreach (var T in listTareas)
+{
+    Console.WriteLine("Titulo:" + T.title + " - " + "Completado:" + T.completed);
+}
+Console.WriteLine("\n");
