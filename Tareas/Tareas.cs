d958be6d2204861941ea9ab7b56944431387af25
Paulo Srur <paulosrur777@gmail.com>
2025-07-09 21:25:40 -0300

    estructura de clase tareas lista para deserializar

---
+++ Tareas/Tareas.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Tarea;
+
+public class Tareas
+{
+    public int userId { get; set; }
+    public int id{get;set;}
+    public string title { get; set; } = string.Empty;
+    public bool completed{get;set;}
+
+}
